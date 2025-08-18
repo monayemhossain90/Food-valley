453352a734402977570ae5a19ac361f0f4e7bf60
monayemhossain90 <monayemhossain1990@gmail.com>
2025-08-18 06:43:36 +0600

    food valley website started

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Navbar.css";
+import {assets} from "../../assets/assets"
+
+const Navbar = () => {
+
+  return (
+
+    <div className='navbar'>
+        <img src={assets.logo} alt="logo" className='logo' />
+        <ul className="navbar-menu">
+            <li>home</li>
+            <li>menu</li>
+            <li>mobile app</li>
+            <li>contact us</li>
+
+        </ul>
+
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="search icon" />
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="basket icon" />
+                <div className="dot"></div>
+            </div>
+            <button>Sign in</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
